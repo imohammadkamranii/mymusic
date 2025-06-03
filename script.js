919f4dc2acb25e0175c1460cddafce3f4b803a16
mohamad kamrani <111233059+imohammadkamranii@users.noreply.github.com>
2025-06-03 22:28:37 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,250 +1,28 @@
-$(function () {
-  var playerTrack = $("#player-track"),
-    bgArtwork = $("#bg-artwork"),
-    bgArtworkUrl,
-    albumName = $("#album-name"),
-    trackName = $("#track-name"),
-    albumArt = $("#album-art"),
-    sArea = $("#s-area"),
-    seekBar = $("#seek-bar"),
-    trackTime = $("#track-time"),
-    insTime = $("#ins-time"),
-    sHover = $("#s-hover"),
-    playPauseButton = $("#play-pause-button"),
-    i = playPauseButton.find("i"),
-    tProgress = $("#current-time"),
-    tTime = $("#track-length"),
-    seekT,
-    seekLoc,
-    seekBarPos,
-    cM,
-    ctMinutes,
-    ctSeconds,
-    curMinutes,
-    curSeconds,
-    durMinutes,
-    durSeconds,
-    playProgress,
-    bTime,
-    nTime = 0,
-    buffInterval = null,
-    tFlag = false,
-    albums = [
-      "radepa",
-      "shaba",
-      " sobgzohrshab",
-      "delkhoshi",
-      "hanoo zende"
-    ],
-    trackNames = [
-      "ho3ein",
-      "ho3ein",
-      "ho3ein",
-      "ho3ein",
-      "ho3ein"
-    ],
-    albumArtworks = ["_1", "_2", "_3", "_4", "_5"],
-    trackUrl = [
-      "https://dl2.limusic.org/Music/1403/07/01/Ho3ein%20-%20Radepa%20(Ft%20Sadegh%20&%20T-Dey).mp3",
-      "https://dl.melodyboss.com/music/rap/ho3ein/singles/Hosein%20-%20Shaba%20(featuring%20Sadegh).mp3",
-      "https://dl.musicguitars.ir/Music/Ho3ein/320/Ho3ein%20-%20Sob%20Zohr%20Shab.mp3",
-      "https://dl.musicguitars.ir/Music/Ho3ein/320/Ho3ein%20Ft%20Bigrez%20-%20Delkhoshi.mp3",
-      "https://serverdl.mediahits.ir/hanoo_zende_sargarm_.mp3"
-    ],
-    playPreviousTrackButton = $("#play-previous"),
-    playNextTrackButton = $("#play-next"),
-    currIndex = -1;
+// URL فایل JSON در GitHub
+const playlistURL = "https://raw.githubusercontent.com/imohammadkamranii/mymusic/main/playlist.json";
 
-  function playPause() {
-    setTimeout(function () {
-      if (audio.paused) {
-        playerTrack.addClass("active");
-        albumArt.addClass("active");
-        checkBuffering();
-        i.attr("class", "fas fa-pause");
-        audio.play();
-      } else {
-        playerTrack.removeClass("active");
-        albumArt.removeClass("active");
-        clearInterval(buffInterval);
-        albumArt.removeClass("buffering");
-        i.attr("class", "fas fa-play");
-        audio.pause();
-      }
-    }, 300);
-  }
+// عناصر DOM
+const audioPlayer = document.getElementById("audioPlayer");
+const playlistElement = document.getElementById("playlist");
 
-  function showHover(event) {
-    seekBarPos = sArea.offset();
-    seekT = event.clientX - seekBarPos.left;
-    seekLoc = audio.duration * (seekT / sArea.outerWidth());
-
-    sHover.width(seekT);
-
-    cM = seekLoc / 60;
-
-    ctMinutes = Math.floor(cM);
-    ctSeconds = Math.floor(seekLoc - ctMinutes * 60);
-
-    if (ctMinutes < 0 || ctSeconds < 0) return;
-
-    if (ctMinutes < 0 || ctSeconds < 0) return;
-
-    if (ctMinutes < 10) ctMinutes = "0" + ctMinutes;
-    if (ctSeconds < 10) ctSeconds = "0" + ctSeconds;
-
-    if (isNaN(ctMinutes) || isNaN(ctSeconds)) insTime.text("--:--");
-    else insTime.text(ctMinutes + ":" + ctSeconds);
-
-    insTime.css({ left: seekT, "margin-left": "-21px" }).fadeIn(0);
-  }
-
-  function hideHover() {
-    sHover.width(0);
-    insTime.text("00:00").css({ left: "0px", "margin-left": "0px" }).fadeOut(0);
-  }
-
-  function playFromClickedPos() {
-    audio.currentTime = seekLoc;
-    seekBar.width(seekT);
-    hideHover();
-  }
-
-  function updateCurrTime() {
-    nTime = new Date();
-    nTime = nTime.getTime();
-
-    if (!tFlag) {
-      tFlag = true;
-      trackTime.addClass("active");
-    }
-
-    curMinutes = Math.floor(audio.currentTime / 60);
-    curSeconds = Math.floor(audio.currentTime - curMinutes * 60);
-
-    durMinutes = Math.floor(audio.duration / 60);
-    durSeconds = Math.floor(audio.duration - durMinutes * 60);
-
-    playProgress = (audio.currentTime / audio.duration) * 100;
-
-    if (curMinutes < 10) curMinutes = "0" + curMinutes;
-    if (curSeconds < 10) curSeconds = "0" + curSeconds;
-
-    if (durMinutes < 10) durMinutes = "0" + durMinutes;
-    if (durSeconds < 10) durSeconds = "0" + durSeconds;
-
-    if (isNaN(curMinutes) || isNaN(curSeconds)) tProgress.text("00:00");
-    else tProgress.text(curMinutes + ":" + curSeconds);
-
-    if (isNaN(durMinutes) || isNaN(durSeconds)) tTime.text("00:00");
-    else tTime.text(durMinutes + ":" + durSeconds);
-
-    if (
-      isNaN(curMinutes) ||
-      isNaN(curSeconds) ||
-      isNaN(durMinutes) ||
-      isNaN(durSeconds)
-    )
-      trackTime.removeClass("active");
-    else trackTime.addClass("active");
-
-    seekBar.width(playProgress + "%");
-
-    if (playProgress == 100) {
-      i.attr("class", "fa fa-play");
-      seekBar.width(0);
-      tProgress.text("00:00");
-      albumArt.removeClass("buffering").removeClass("active");
-      clearInterval(buffInterval);
-    }
-  }
-
-  function checkBuffering() {
-    clearInterval(buffInterval);
-    buffInterval = setInterval(function () {
-      if (nTime == 0 || bTime - nTime > 1000) albumArt.addClass("buffering");
-      else albumArt.removeClass("buffering");
-
-      bTime = new Date();
-      bTime = bTime.getTime();
-    }, 100);
-  }
-
-  function selectTrack(flag) {
-    if (flag == 0 || flag == 1) ++currIndex;
-    else --currIndex;
-
-    if (currIndex > -1 && currIndex < albumArtworks.length) {
-      if (flag == 0) i.attr("class", "fa fa-play");
-      else {
-        albumArt.removeClass("buffering");
-        i.attr("class", "fa fa-pause");
-      }
-
-      seekBar.width(0);
-      trackTime.removeClass("active");
-      tProgress.text("00:00");
-      tTime.text("00:00");
-
-      currAlbum = albums[currIndex];
-      currTrackName = trackNames[currIndex];
-      currArtwork = albumArtworks[currIndex];
-
-      audio.src = trackUrl[currIndex];
-
-      nTime = 0;
-      bTime = new Date();
-      bTime = bTime.getTime();
-
-      if (flag != 0) {
-        audio.play();
-        playerTrack.addClass("active");
-        albumArt.addClass("active");
-
-        clearInterval(buffInterval);
-        checkBuffering();
-      }
-
-      albumName.text(currAlbum);
-      trackName.text(currTrackName);
-      albumArt.find("img.active").removeClass("active");
-      $("#" + currArtwork).addClass("active");
-
-      bgArtworkUrl = $("#" + currArtwork).attr("src");
-
-      bgArtwork.css({ "background-image": "url(" + bgArtworkUrl + ")" });
-    } else {
-      if (flag == 0 || flag == 1) --currIndex;
-      else ++currIndex;
-    }
-  }
-
-  function initPlayer() {
-    audio = new Audio();
-
-    selectTrack(0);
-
-    audio.loop = false;
-
-    playPauseButton.on("click", playPause);
-
-    sArea.mousemove(function (event) {
-      showHover(event);
+// بارگذاری فایل JSON و نمایش لینک‌ها
+fetch(playlistURL)
+    .then(response => {
+        if (!response.ok) throw new Error("Unable to fetch playlist.json");
+        return response.json();
+    })
+    .then(playlist => {
+        playlist.forEach((track, index) => {
+            const li = document.createElement("li");
+            li.textContent = `Track ${index + 1}`;
+            li.addEventListener("click", () => {
+                audioPlayer.src = track;
+                audioPlayer.play();
+            });
+            playlistElement.appendChild(li);
+        });
+    })
+    .catch(error => {
+        console.error("Error fetching playlist:", error);
+        playlistElement.innerHTML = "<li>Unable to load playlist</li>";
     });
-
-    sArea.mouseout(hideHover);
-
-    sArea.on("click", playFromClickedPos);
-
-    $(audio).on("timeupdate", updateCurrTime);
-
-    playPreviousTrackButton.on("click", function () {
-      selectTrack(-1);
-    });
-    playNextTrackButton.on("click", function () {
-      selectTrack(1);
-    });
-  }
-
-  initPlayer();
-});
